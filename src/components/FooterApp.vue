ee9364f92dae456e5fa381fd378a34958ff762a8
ElisabettaMirti <belmirti@gmail.com>
2024-06-19 16:21:38 +0200

    added data for footer nav

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -2,7 +2,71 @@
 export default{
     data() {
         return{
-            
+            quickLinksNav : [
+                {
+                    id: 1,
+                    title: 'Games'
+                },
+                {
+                    id: 2,
+                    title: 'Latest Tournament'
+                },
+                {
+                    id: 3,
+                    title: 'Shop'
+                },
+                {
+                    id: 4,
+                    title: 'Blog'
+                },
+                {
+                    id: 5,
+                    title: 'Contact'
+                },
+            ],
+
+            helpNav : [
+                {
+                    id: 1,
+                    title: 'Faqs'
+                },
+                {
+                    id: 2,
+                    title: 'Terms & Condition'
+                },
+                {
+                    id: 3,
+                    title: 'Privacy'
+                }
+            ],
+
+            socialNav : [
+                {
+                    id: 1,
+                    title: 'Facebook'
+                },
+                {
+                    id: 2,
+                    title: 'Twitter'
+                },
+                {
+                    id: 3,
+                    title: 'Instagram'
+                },
+                {
+                    id: 4,
+                    title: 'Youtube'
+                },
+                {
+                    id: 5,
+                    title: 'Github'
+                },
+            ],
+
+            contactNav : {
+                address: '4 Mayfield, Buckden, PE19 5SZ',
+                phone: '(302)555-0107'
+            }
         }
     }
 }
