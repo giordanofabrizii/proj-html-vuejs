904a0bf0e81298939bf91aee6b2b9a023be6890b
giordanofabrizii <giordanofabrizi@gmail.com>
2024-06-19 12:25:34 +0200

    Added first elements

--- src/components/ButtonApp.vue
+++ src/components/ButtonApp.vue
@@ -4,14 +4,45 @@ export default{
         return{
             
         }
+    },
+    props:{
+        name: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        },
     }
 }
 </script>
 
 <template>
-    <h1>Button</h1>
+    <div class="surround">
+        <button>
+            <a :href="url">{{ name }}</a>
+        </button>
+    </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+
+.surround{
+    border: 2px solid $green;
+    padding: .5rem;
+    border-radius: 5rem;
+    display: flex;
+    align-items: center;
+    width: auto;
+
+    button{
+        border: none;
+        background-color: $green;
+        padding: .5rem;
+        border-radius: 5rem;
+    }
+}
 
 </style>
--- src/components/MainThirdSection.vue
+++ src/components/MainThirdSection.vue
@@ -1,15 +1,24 @@
 <script>
+import ButtonApp from './ButtonApp.vue';        
+import CircleButtonApp from './CircleButtonApp.vue';
+
 export default{
     data() {
         return{
             
         }
+    },
+    components:{
+        ButtonApp,
+        CircleButtonApp
     }
 }
 </script>
 
 <template>
     <h1>Futio</h1>
+    <ButtonApp :name="'Bottone'" :url="'https://www.google.com'"/>
+    <CircleButtonApp/>
     <!-- Quick fats -->
 </template>
 
