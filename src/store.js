c96ce3d7f957cf4f26f8cb47f0a684e6a649a526
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-21 11:26:10 +0200

    Dato nello store carrello.

--- src/store.js
+++ src/store.js
@@ -1,5 +1,30 @@
 import {reactive} from 'vue';
 
 export const store = reactive ({
-    
+    shopList:{
+        items: [
+            {
+                name: 'Havit RGB Headphone',
+                title: 'Cuffie RGB Havit',
+                price: '$380.00',
+                oldPrice: '$410.00',
+                url: 'src/assets/img/shop-image-3.png',
+                count: 1,
+            },{
+                name: 'Touch Controller Grip',
+                title: 'Controller grip touch',
+                price: '$380.00',
+                oldPrice: '$410.00',
+                url: 'src/assets/img/shop-image-5.png',
+                count: 1,
+            },{
+                name: 'Gaming Microphone',
+                title: 'Microfono da gaming',
+                price: '$380.00',
+                oldPrice: '$410.00',
+                url: 'src/assets/img/shop-image-8.png',
+                count: 1,
+            },
+        ],
+    },
 })
