45794353654652b8cefd69e9f487b15c4c904202
ElisabettaMirti <belmirti@gmail.com>
2024-06-21 10:10:45 +0200

    button animation finished

--- src/components/CircleButtonApp.vue
+++ src/components/CircleButtonApp.vue
@@ -16,52 +16,69 @@ export default{
 
 <template>
     <div class="bttn-container">
-        <div class="surround">
-            <button>
-                <a :href="url">
-                    <i class="fa-solid fa-play"></i>
-                </a>
-            </button>
-        </div>
+        <button>
+            <div class="surround">
+            </div>
+            <a :href="url">
+                <i class="fa-solid fa-play"></i>
+            </a>
+        </button>
+        
     </div>
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/partials/variables' as *;
 
+div.bttn-container{
+    margin-bottom: 2rem;
+}
+
     .surround{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border: 2px solid $green;
-    transition: all .3s linear;
+    border: 1px solid $green;
     margin: .5rem;
     padding: .3rem .5rem;
     border-radius: 5rem;
-    width: 4rem;
+    width: 5rem;
     aspect-ratio: 1/1;
-    animation: border 2s linear infinite;
+    animation: my-animation 4s infinite;
+    position: absolute;
+}
 
-    button{
-        // background-color: red;
-        border: none;
-        background-color: $white;
-        transition: all .3s linear;
-        padding: .9rem;
-        border-radius: 5rem;
-        display: flex;
-        align-items: center;
-        width: 3rem;
-        aspect-ratio: 1/1;
+button{
+    // background-color: red;
+    border: none;
+    background-color: $white;
+    padding: 2rem;
+    border-radius: 5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 3rem;
+    aspect-ratio: 1/1;
+    position: relative;
+    cursor: pointer;
 
-            a{
-                text-transform: uppercase;
-                font-weight: 500;
-                font-size: 1.2rem;
-                color: $green;
-            }
+        a{
+            text-transform: uppercase;
+            font-weight: 500;
+            font-size: 1.2rem;
+            color: $green;
+            
         }
     }
+    
 
+    @keyframes my-animation {
+        0%{
+            transform: scale(1, 1);
+        }
+        50%{
+            transform: scale(1.2, 1.2);
+        }
+        100%{
+            transform: scale(1, 1);
+        }
+    }
 
 </style>
