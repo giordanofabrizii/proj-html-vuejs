4cc18f1ee617dd882e3b621bc5aa58feecce7360
giordanofabrizii <giordanofabrizi@gmail.com>
2024-06-21 13:06:46 +0200

    added scrollbuttonup

---
+++ src/components/ScrollUpButtonApp.vue
@@ -0,0 +1,47 @@
+<script>
+export default{
+    data() {
+        return{
+            scroll: null,
+        }
+    },
+    methods: {
+        updateScroll() {
+            this.scroll = window.scrollY
+        },
+    },
+    mounted() {
+        window.addEventListener('scroll', this.updateScroll);
+    }
+}
+</script>
+
+<template>
+    <button :class="(scroll > 100) ? 'visible' : null">
+        <img src="../assets/img/svg/e-double-right-arrow.svg" alt="">
+    </button>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+
+    button{
+        position: fixed;
+        top: 105vh;
+        right: 2rem;
+        background-color: $light-green;
+        z-index: 3;
+        border: none;
+        padding: 1rem;
+        border-radius: 8px;
+        transition: top 1s linear;
+
+        img{
+            transform: rotate(-90deg);
+        }
+    
+        &.visible{
+            top: calc(100vh - 5rem);
+        }
+    }
+</style>
