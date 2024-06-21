243c7a7d4d558ec0837ae591246421d5b6866dc6
ElisabettaMirti <belmirti@gmail.com>
2024-06-21 10:33:04 +0200

    added new component

---
+++ src/components/TournamentSection.vue
@@ -0,0 +1,20 @@
+<script>
+export default{
+    data() {
+        return{
+            
+        }
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss';
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+
+</style>
